1ee9c606688181aafe3c7ca81e7268b225d3f66f
Kallinikos Milonakis <kallinikosmil@gmail.com>
2025-10-18 15:35:00 +0300

    wtf is going on

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot } from 'expo-router';
-import AppProviders from './core/AppProviders';
+import AppProviders from '../modules/core/AppProviders';
 
 export default function RootLayout() {
   return (
